d007332a1f68211a4ad5590ce0df68929db8c16f
Bru3414 <bruno3414.dambroski@gmail.com>
2023-06-22 09:08:53 -0400

    adicionando a seção FAQ

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -12,7 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (var i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', hideOpenQuestion);
+    }
+
 })
+
+function hideOpenQuestion(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe);
+}
 
 function hideButtonActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
