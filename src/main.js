38d62b94438d0418cfcb57b868c5fb01ec167b70
Bru3414 <bruno3414.dambroski@gmail.com>
2023-06-23 10:41:37 -0400

    adicionando header e css responsivo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    const altHero = document.querySelector('.hero').clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const positionY = window.scrollY;
+
+        if (positionY < altHero) {
+            hideHeader();
+        }else {
+            exibeHeader();
+        }
+    })
+
+    // Seção de atrações, programação das abas
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,11 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Seção FAQ, accordion
     for (var i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', hideOpenQuestion);
     }
 
 })
+
+function hideHeader() {
+    const header = document.querySelector('header');
+
+    header.classList.add('header--is-hidden');
+}
+
+function exibeHeader() {
+    const header = document.querySelector('header');
+
+    header.classList.remove('header--is-hidden');
+}
 
 function hideOpenQuestion(elemento) {
     const classe = 'faq__questions__item--is-open';
